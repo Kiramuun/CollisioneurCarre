861219457b24d4ba0c1200c5939fb005ae68d926
Kiramuun <deimonlight@live.fr>
2024-01-05 09:40:40 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using CollisioneurCarré;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+InitWindow(500, 500, "Hello World");
+
+SetTargetFPS(1);
+
+Drawing main = new Drawing();
+main.Init();
+
+while (!WindowShouldClose())
+{
+    BeginDrawing();
+
+    ClearBackground(Color.WHITE);
+    main.Auto();
+    main.Render();
+
+    EndDrawing();
+}
+
+CloseWindow();
